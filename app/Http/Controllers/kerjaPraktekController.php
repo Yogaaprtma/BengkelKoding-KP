a954e48417487399f12f970dd25407174b556a8b
Yoga Adi Pratama <yogapratama010503@gmail.com>
2024-05-12 00:03:55 +0700

    Menambah fitur form tambah pengajuan sama form draft pengajuan

--- app/Http/Controllers/kerjaPraktekController.php
+++ app/Http/Controllers/kerjaPraktekController.php
@@ -8,6 +8,16 @@ class kerjaPraktekController extends Controller
 {
     public function index()
     {
-        return view('daftarDosen');
+        return view('mahasiswa.daftarDosen');
+    }
+
+    public function tambahPengajuan()
+    {
+        return view('mahasiswa.form_tambahPengajuan');
+    }
+
+    public function draftPengajuan()
+    {
+        return view('mahasiswa.form_draftPengajuan');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', function () {
 });
 
 Route::get('/halamanDosen', [kerjaPraktekController::class, 'index'])->name('halaman-dosen');
+Route::get('/tambah-pengajuan-bimbingan', [kerjaPraktekController::class, 'tambahPengajuan'])->name('tambah_pengajuan');
+Route::get('/draft-pengajuan-bimbingan', [kerjaPraktekController::class, 'draftPengajuan'])->name('draft_pengajuan');
